23db9464039075e64b75a6b213a955d3f7ca4fec
Rurutia1027 <rurutia1027@gmail.com>
2025-12-30 14:14:48 +0800

    update scenario 3 loading period into 2mins

--- src/scenario-3.js
+++ src/scenario-3.js
@@ -3,7 +3,7 @@ import { check, sleep } from 'k6';
 
 export let options = {
     vus: 100,             // number of virtual users
-    duration: '10m',      // test duration
+    duration: '2m',      // test duration
     thresholds: {
         http_req_duration: ['p(90)<1000'],  // 90% requests under 1s
         'http_req_failed': ['rate<0.05'],  // allow up to 5% failures under stress
